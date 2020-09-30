c0e6c248fcddd7cd80a1b835a7ecfbfbf8385851
diegogar03 <70651719+diegogar03@users.noreply.github.com>
2020-09-30 10:56:00 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+let light2 = 0
+basic.forever(function () {
+    light2 = input.lightLevel()
+    if (light2 <= 85) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (light2 > 85 && light2 <= 170) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (light2 > 170 && light2 <= 255) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
